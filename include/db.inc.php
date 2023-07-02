9f44a67527d61a18428c3f76526ee0beeb9dec75
Hans Schneider <hansschneider@me.com>
2023-07-02 13:12:29 +0200

    moved base configvariables from inc_db to inc_base

--- include/db.inc.php
+++ include/db.inc.php
@@ -1,21 +1,5 @@
 <?php
-$basedir = $_SERVER['DOCUMENT_ROOT'];
-require_once($basedir.'../../DO_NOT_DEPLOY.php');
-
-error_reporting(E_ERROR  | E_PARSE);
-
-// todo: move to own file
-
-$serverHost = "99cf-95-89-45-59.ngrok-free.app";
-$serverName = "https://".$serverHost;
-$stravaRedirectURI = $serverName.'/members/auth_strava.php';
-
-
-
-
-require_once($basedir.'/vendor/autoload.php');
-
-
+require_once($_SERVER['DOCUMENT_ROOT'].'/include/lib_base.inc.php');
 use Opis\Database\Connection;
 
 use Opis\ORM\{
---
+++ include/lib_base.inc.php
@@ -0,0 +1,21 @@
+<?php
+
+$basedir = $_SERVER['DOCUMENT_ROOT'];
+require_once($basedir.'../../DO_NOT_DEPLOY.php');
+
+error_reporting(E_ERROR  | E_PARSE);
+
+// todo: move to own file
+
+$serverHost = "99cf-95-89-45-59.ngrok-free.app";
+$serverName = "https://".$serverHost;
+$stravaRedirectURI = $serverName.'/members/auth_strava.php';
+
+
+
+
+require_once($basedir.'/vendor/autoload.php');
+
+
+
+?>
